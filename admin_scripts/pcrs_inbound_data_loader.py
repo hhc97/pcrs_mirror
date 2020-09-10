2e79f9f2bbc8f94acbe53e6f355b9610f53c1b31
Zain Kazmi <zkazmi786@hotmail.ca>
2020-09-10 12:44:40 -0400

    pcrs inbound data loader update

--- admin_scripts/pcrs_inbound_data_loader.py
+++ admin_scripts/pcrs_inbound_data_loader.py
@@ -3,17 +3,20 @@ import sys
 
 conn = psycopg2.connect(database='pcrs', host="pcrsdev.utm.utoronto.ca", user='waf', password='hehexd') #put pw here
 
+infile = open('top_n_questions_fo_users', 'rb')
+rec_data = pickle.load(infile)
+infile.close()
+
 dbCursor = conn.cursor() # opening conenction to db
 
 sqlInsertRow = "INSERT INTO fixit_problemrecommendedfixit values(%s, %s, %s, %s)" # query, inserting into fixit
 
-# row id, problem type, pid, uid
-
-dbCursor.execute(sqlInsertRow, (5, 'multiple_choice', 2, 1))
+for rec_problem in rec_data:
+    # problem type, pid, uid
+    dbCursor.execute(sqlInsertRow, rec_data)
 
 conn.commit()
 
-# create a database in ur postgreql table.
 
 
 
