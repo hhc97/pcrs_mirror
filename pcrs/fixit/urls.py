93a627110f030c0054465a952abe549a0facedc2
Zain Kazmi <zkazmi786@hotmail.ca>
2020-09-12 15:46:34 -0400

    Add API route to urls.py

--- pcrs/fixit/urls.py
+++ pcrs/fixit/urls.py
@@ -1,11 +1,17 @@
-from django.conf.urls import url 
+from django.conf.urls import include, url
 
 from .views import *
 
+from . import views
 from problems.views import *
 from problems_multiple_choice.forms import ProblemForm
 from problems_multiple_choice.models import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'StudentFixitProfile', StudentFixitProfileViewSet)
 
 urlpatterns = [
     url(r'^main/', studentFixitView.as_view()),
+    url(r'^api/', include(router.urls))
 ]
