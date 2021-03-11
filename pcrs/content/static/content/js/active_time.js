c2ae9bd6516755ddace78d7acebabd51d8155938
Andrew Petersen <andrew.petersen@utoronto.ca>
2021-03-11 09:34:27 -0500

    Pushing active time on every lost focus

--- pcrs/content/static/content/js/active_time.js
+++ pcrs/content/static/content/js/active_time.js
@@ -1,9 +1,8 @@
 window.addEventListener('focus', gotFocus);
-window.addEventListener('unload', cpUnloaded);
+window.addEventListener('unload', lostFocus);
 window.addEventListener('blur', lostFocus);
 
 var start_time = new Date().getTime();
-var elapsed_time = 0;
 
 function gotFocus() {
     start_time = new Date().getTime();
@@ -11,12 +10,7 @@ function gotFocus() {
 
 function lostFocus() {
     var curr_time = new Date().getTime();
-    elapsed_time = elapsed_time + (curr_time - start_time);
-}
-
-function cpUnloaded() {
-    var curr_time = new Date().getTime();
-    elapsed_time = elapsed_time + (curr_time - start_time);
+    var elapsed_time = curr_time - start_time;
     $.post(window.location.href + "/activetime",
         {csrftoken: csrftoken, elapsed_time: elapsed_time});
 }
