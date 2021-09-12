b69b25889fe4064965c556aacaa564a876367345
Andrew Petersen <andrew.petersen@utoronto.ca>
2021-09-12 16:03:33 -0400

    First commit in a series: gradually refining error messages shown to students

--- pcrs/resources/csrf_protection.js
+++ pcrs/resources/csrf_protection.js
@@ -1,5 +1,14 @@
-$(document).ajaxError(function () {
-    alert('Error: Action did not complete. Usually, this occurs when authentication expires. Try loading PCRS in another tab to refresh your session, and then try the action again. If you are submitting an exercise, do not forget to copy (and save) the code, in case your page is reloaded.');
+$(document).ajaxError(function (event, jqXHR, settings, thrownError) {
+    switch (jqXHR.status) {
+    case 0:
+        alert('Error: A network request could not be completed, either because of a local javascript error or because the network is down or very slow. If you are submitting an exercise, copy (and save) the code, so you do not lose work.');
+        break;
+    case 500:
+        alert('Error: PCRS request failed due to internal server error. Please contact your instructor with the time of the request, so they can investigate. Also, save any code you are submitting so you do not lose your work.');
+        break;
+    default:
+        alert('Error: Action did not complete. Usually, this occurs when authentication expires. Try loading PCRS in another tab to refresh your session, and then try the action again. If you are submitting an exercise, do not forget to copy (and save) the code, in case your page is reloaded.');
+    }
 });
 
 var csrftoken = getCookie('csrftoken');
