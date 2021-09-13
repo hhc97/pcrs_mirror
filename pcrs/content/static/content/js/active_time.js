645d3a422d854a898c650de10fc017df4b01a718
Andrew Petersen <andrew.petersen@utoronto.ca>
2021-09-13 00:29:17 -0400

    Fix for track_time not catching exiting page to visit a new page

--- pcrs/content/static/content/js/active_time.js
+++ pcrs/content/static/content/js/active_time.js
@@ -12,8 +12,14 @@ function lostFocus() {
     var curr_time = new Date().getTime();
     var elapsed_time = curr_time - start_time;
     var location = window.location.href.split('#')[0];
-    $.post(location + "/activetime",
-        {csrftoken: csrftoken, elapsed_time: elapsed_time})
-     .fail(function(response) {
-        console.log("Error: Cannot log elapsed time")});         // Likely lost authentication, so we'll lose that data.
+    //$.post(location + "/activetime",
+    //    {csrftoken: csrftoken, elapsed_time: elapsed_time})
+    let data = new FormData();
+    data.append('csrftoken', csrftoken);
+    data.append('csrfmiddlewaretoken', csrftoken);
+    data.append('elapsed_time', elapsed_time);
+    navigator.sendBeacon(location + "/activetime", data);
+
+    // header:
+    // X-CSRFToken: vDtM0dRbFptcNdQmDiv96cXiYGU3vVXwjfWat9rdJ0bDEPSLHIMHGfaj47gGfG5e
 }
