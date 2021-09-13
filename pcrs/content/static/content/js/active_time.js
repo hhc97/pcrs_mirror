71bd978d6e69a509a0849299c5f7d89c047d8435
Andrew Petersen <andrew.petersen@utoronto.ca>
2021-09-13 09:55:28 -0400

    cleanup of js file

--- pcrs/content/static/content/js/active_time.js
+++ pcrs/content/static/content/js/active_time.js
@@ -12,14 +12,9 @@ function lostFocus() {
     var curr_time = new Date().getTime();
     var elapsed_time = curr_time - start_time;
     var location = window.location.href.split('#')[0];
-    //$.post(location + "/activetime",
-    //    {csrftoken: csrftoken, elapsed_time: elapsed_time})
     let data = new FormData();
     data.append('csrftoken', csrftoken);
     data.append('csrfmiddlewaretoken', csrftoken);
     data.append('elapsed_time', elapsed_time);
     navigator.sendBeacon(location + "/activetime", data);
-
-    // header:
-    // X-CSRFToken: vDtM0dRbFptcNdQmDiv96cXiYGU3vVXwjfWat9rdJ0bDEPSLHIMHGfaj47gGfG5e
 }
